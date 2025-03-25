aed75405b940f46abacce0efe9ab600a32f7991a
kaweendras <salitha.wijerathna@gmail.com>
2025-03-25 16:49:52 +0530

    CHG | configure CORS middleware to allow requests from all origins with specified methods

--- index.ts
+++ index.ts
@@ -10,7 +10,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+); // Allow CORS from all origins
 
 // Connect to database
 connectDB();
