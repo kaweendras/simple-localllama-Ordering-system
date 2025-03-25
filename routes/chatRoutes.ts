34cb713766cdcab7e67d0a525d685ce76cc48a87
kaweendras <salitha.wijerathna@gmail.com>
2025-03-25 12:32:13 +0530

    CHG | add user-response route to handle user responses in chat

--- routes/chatRoutes.ts
+++ routes/chatRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import handleChat from "../controllers/chatController";
+import { handleChat, handleUserResponse } from "../controllers/chatController";
 
 const router = express.Router();
 
 router.post("/chat", handleChat);
+
+router.post("/user-response", handleUserResponse);
 
 export default router;
