f9f2dc7dcd2f380d44b55c72ca28e2e792d07686
kaweendras <salitha.wijerathna@gmail.com>
2025-03-26 16:17:05 +0530

    CHG | update user schema to enforce unique userId constraint

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  userId: { type: String, required: true },
+  userId: { type: String, unique: true, required: true },
   fname: { type: String, required: true },
   lname: { type: String, required: true },
   email: { type: String, required: true },
